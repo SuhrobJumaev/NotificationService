0eff52c9dda157065f0858dc724e819c6d405414
= <=>
2024-06-01 19:45:16 +0500

    Added NpgSqlFactory for creation db connections and simple DbIntializer

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using NotificationService.Factories;
+using NotificationService.Helpers;
+using NotificationService.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var config = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+           new NpgsqlConnectionFactory(config.GetSection(Utils.ConnectionStringsSectionName)[Utils.DefaultConnectionKeyName]!));
+
+builder.Services.AddSingleton<DbInitializer>();
+
 
 var app = builder.Build();
 
@@ -17,9 +30,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
 
 app.Run();
